0f8b0e5dc78ada73acc2c3184d930e78b0891170
M1ngos <mingoemanuel07@gmail.com>
2023-02-23 11:52:25 -0500

    organizing

--- main.py
+++ main.py
@@ -1,31 +1,6 @@
 #! /usr/bin/env python3
 
 import os
+import argparse
 
-def showMenu():  
- print("""
-1.MAC Changer
-2.Network Manager       
-3.ARP spoofer        
-4.Lazagne      
-5.Exit """)
-
-def selection(op):
-    if op == "1":
-      interface = input("Interface to use:")
-      mac = input("Input the new mac:")
-      print("MAC changer")
-      os.system("python3 mac_changer.py -i"+interface+" -m"+mac)
-
-    elif op == "2": 
-      exit()
-
-print("----------------------")
-print("Swiss-Messer ver.0.1")
-print("Ctrl+c to Exit!")
-print("----------------------")
-
-while True:
-    showMenu()
-    option = input("Chose an option:")
-    selection(option)
+parser = argparse.ArgumentParser(prog='messer.py',add_help=False)
